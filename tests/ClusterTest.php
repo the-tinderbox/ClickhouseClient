af4c398dea3b8dc49e1ca1247e80bc9fddfc5d90
Roman Pravda <rp@imagespark.ru>
2020-09-16 16:21:44 +0300

    fix test

--- tests/ClusterTest.php
+++ tests/ClusterTest.php
@@ -112,14 +112,9 @@ class ClusterTest extends TestCase
         $this->assertEquals($server['password'], $createdServer->getPassword());
         $this->assertEquals($server['options'], $createdServer->getOptions());
 
-        $createdServer = $cluster->getServersByTag('tag');
+        $servers = $cluster->getServersByTag('tag');
 
-        $this->assertEquals($server['host'], $createdServer->getHost());
-        $this->assertEquals($server['port'], $createdServer->getPort());
-        $this->assertEquals($server['database'], $createdServer->getDatabase());
-        $this->assertEquals($server['username'], $createdServer->getUsername());
-        $this->assertEquals($server['password'], $createdServer->getPassword());
-        $this->assertEquals($server['options'], $createdServer->getOptions());
+        $this->assertEquals(['127.0.0.1' => $createdServer], $servers);
     }
 
     public function testServerTagNotFound()
